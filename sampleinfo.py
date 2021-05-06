6f01d161979dca3092c6ff00243dc4e0e2dece31
Stas Rirak <stasrira@yahoo.com>
2021-05-06 10:46:36 -0400

    Bug fixes for running through wsgi.py

--- sampleinfo.py
+++ sampleinfo.py
@@ -2,13 +2,14 @@ from app import app
 from os import environ, path
 from dotenv import load_dotenv
 
+# load environment variables from files
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.flaskenv'))
+load_dotenv(path.join(basedir, '.env'))
+
+
 if __name__ == "__main__":
     # load_dotenv('.flaskenv')
-
-    # load environment variables from files
-    basedir = path.abspath(path.dirname(__file__))
-    load_dotenv(path.join(basedir, '.flaskenv'))
-    load_dotenv(path.join(basedir, '.env'))
 
     # get host and port for running flask
     host = environ.get('FLASK_RUN_HOST')
