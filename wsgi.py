dcf06feb067b967a134088d8494ed59ed29cc534
Stas Rirak <stasrira@yahoo.com>
2021-05-06 16:37:21 -0400

    Adds examples of the files required for setting up the app on the Ubuntu 20.04 server.

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
 from sampleinfo import app
-from os import environ, path
-from dotenv import load_dotenv
 
 if __name__ == "__main__":
-    # # load environment variables from files
-    # basedir = path.abspath(path.dirname(__file__))
-    # load_dotenv(path.join(basedir, '.flaskenv'))
-    # load_dotenv(path.join(basedir, '.env'))
-
     app.run()
